304d4d00ae9e6e78cc03ddd4f7cdbcd3c9d45c0a
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-01 13:18:45 -0800

    working map

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # This project is aimed to showcase React and Data Visualization
 
-## ``` npm install ```
-## ``` npm run start ```
+## `npm install`
 
+## `npm run start`
 
+## In a different terminal
+
+## `cd server`
+
+## `npm run start`
 
 <img src="./src/img/home.png" width="500px">
 <img src="./src/img/fallout.png" width="500px">
 
-## If global map isn't rendering! 
+## If global map isn't rendering!
 
-  * Go here and see if web gl is supported
-   http://mapbox.github.io/mapbox-gl-supported
-   * If not, and you're using chrome, do this...
+* Go here and see if web gl is supported
+  http://mapbox.github.io/mapbox-gl-supported
+* If not, and you're using chrome, do this...
 
-   * Navigate to chrome://flags/
-   * Enable 'Override software rendering list'
+* Navigate to chrome://flags/
+* Enable 'Override software rendering list'
+
+## .env config
+
+* You need to make a .env file and place it inside the root of the server folder
+* It should look like this
+
+  * DARKSKY_SECRET_KEY='darkSkySecretKeyHere'
+
+* You also need to make a .env file and place it at the root of this project folder
+* It should look like this
+
+  * REACT_APP_MAP_BOX_TOKEN='mapBoxTokenKeyHere'
--- src/components/TestMap.js
+++ src/components/TestMap.js
@@ -1,26 +1,93 @@
 import React from "react";
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import { connect } from "react-redux";
 
 const Map = ReactMapboxGl({
-  accessToken:
-    "pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A"
+  accessToken: `${process.env.REACT_APP_MAP_BOX_TOKEN}`
 });
 
 class TestMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      longitude: 2.2137,
+      latitude: 46.22276,
+      dynamicZoom: 1
+    };
+  }
+  componentDidMount() {}
+  componentDidUpdate() {
+    // console.log("app updated", this.state);
+    // try {
+    //   console.log("map updated");
+    //   this.map.state.map.flyTo({
+    //     center: [this.props.weather.longitude, this.props.weather.latitude]
+    //   });
+    // } catch (error) {
+    //   console.log("no map yet", error);
+    // }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // console.log("********************", nextProps, nextProps.weather.length);
+    nextProps.weather.length == undefined &&
+    nextProps.weather.longitude !== this.state.longitude
+      ? this.setState({
+          longitude: nextProps.weather.longitude,
+          latitude: nextProps.weather.latitude,
+          dynamicZoom: 12
+        })
+      : undefined;
+  }
   render() {
     return (
-      <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
-          height: "100vh",
-          width: "100vw"
-        }}
-      >
-        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-        </Layer>
-      </Map>
+      <div>
+        {/* {this.props.weather.latitude !== undefined ? ( */}
+        <Map
+          ref={e => {
+            this.map = e;
+          }}
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw"
+          }}
+          center={[this.state.longitude, this.state.latitude]}
+          zoom={[this.state.dynamicZoom]}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}
+          >
+            {/* <Feature coordinates={[2.2137, 46.22276]} /> */}
+          </Layer>
+        </Map>
+        {/* ) : (
+          <h1>NO location data yet.</h1>
+        )} */}
+      </div>
+
+      //       // fly with default options to null island
+      // map.flyTo({center: [0, 0], zoom: 9});
+      // // using flyTo options
+      // map.flyTo({
+      //   center: [0, 0],
+      //   zoom: 9,
+      //   speed: 0.2,
+      //   curve: 1,
+      //   easing(t) {
+      //     return t;
+      //   }
+      // });
     );
   }
 }
-export default TestMap;
+let mapStateToProps = state => ({
+  weather: state.weather,
+  location: state.location
+});
+
+let mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TestMap);
