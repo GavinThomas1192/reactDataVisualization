40e111553829b461229f560c73ae3ad27f8bb220
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-09 12:51:14 -0800

    setup geolocation proxy to ping my backend instead of client side

--- src/Actions/location-actions.js
+++ src/Actions/location-actions.js
@@ -18,26 +18,17 @@ export const locationUpdate = location => ({
 
 export const getGeoLocation = zip => dispatch => {
   axios
-    .get(
-      `http://maps.googleapis.com/maps/api/geocode/json?address=${zip}&sensor=true`
-    )
+    .post("http://localhost:3001/api/getZip", {
+      zipCode: zip
+    })
     .then(response => {
-      try {
-        console.log(
-          "Response from google geolocation api based on zip code",
-          response.data.results[0].geometry.location,
-          response.data.results[0].formatted_address
-        );
-        let specificGeoAdress = {
-          name: response.data.results[0].formatted_address,
-          latLong: response.data.results[0].geometry.location
-        };
-        dispatch(locationSet(specificGeoAdress));
-      } catch (error) {
-        return console.log("NOT A VALID ZIPCODE! error");
-      }
+      console.log("response from server geolocation info", response);
+      dispatch(locationSet(response.data));
     })
     .then(() => {
       dispatch(getWindData());
+    })
+    .catch(err => {
+      console.log("Internal server error with geolocation", err);
     });
 };
