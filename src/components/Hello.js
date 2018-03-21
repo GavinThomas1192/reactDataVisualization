b5e4253fef0b21ba800ff893c7759f5294525b7e
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-20 19:15:54 -0700

    pure function test

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// export default class Hello extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  render() {
-    return <p>Hello, {this.props.name}!</p>;
-  }
-}
+//   render() {
+//     return <p>Hello, {this.props.name}!</p>;
+//   }
+// }
+
+const Hello = props => {
+  return <p>Hello, {props.name}!</p>;
+};
+
+export default Hello;
