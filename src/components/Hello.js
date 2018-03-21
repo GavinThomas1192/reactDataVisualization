04b1dd670031efd52241e955b419d37acfd9dc7d
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-20 19:25:39 -0700

    default props is lifesaver

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // export default class Hello extends React.Component {
 //   constructor(props) {
@@ -10,8 +11,20 @@ import React from "react";
 //   }
 // }
 
+// const Hello = props => {
+//   return <p>Hello, {props.name === undefined ? "Unknown" : props.name}!</p>;
+// };
+
 const Hello = props => {
   return <p>Hello, {props.name}!</p>;
+};
+
+Hello.propTypes = {
+  name: PropTypes.string
+};
+
+Hello.defaultProps = {
+  name: "Unknown"
 };
 
 export default Hello;
