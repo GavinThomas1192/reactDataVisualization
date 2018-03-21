3e9490ee39f1e6fb2c4df78f9e66a521fc1dc628
GavinThomas1192 <gthomas1192@gmail.com>
2018-03-20 19:20:31 -0700

    coverages percentage

--- src/components/Hello.test.js
+++ src/components/Hello.test.js
@@ -2,7 +2,12 @@ import React from "react";
 import Hello from "./Hello";
 import { shallow } from "enzyme";
 
-it("renders", () => {
+it("renders with the provided name", () => {
   const wrapper = shallow(<Hello name="Jack" />);
   expect(wrapper.find("p").text()).toEqual("Hello, Jack!");
+});
+
+it('renders "unknown" if no name is provided', () => {
+  const wrapper = shallow(<Hello />);
+  expect(wrapper.find("p").text()).toEqual("Hello, Unknown!");
 });
